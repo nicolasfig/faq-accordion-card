1129510f96addb7841f6cad652af47b3415960ed
Nicolas Figueroa <nicolasfigb@gmail.com>
2023-01-02 23:58:41 -0500

    Initial project setup with tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html, script.js"],
+  theme: {
+    colors: {
+      "very-dark-desaturated-blue": "hsl(238, 29%, 16%)",
+      "soft-red": "hsl(14, 88%, 65%)",
+      "soft-violet": "hsl(273, 75%, 66%)",
+      "soft-blue": "hsl(240, 73%, 65%)",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
