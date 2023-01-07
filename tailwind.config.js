f4a9e98d516bd10c5e5d98a4e794cddb3c25f682
Nicolas Figueroa <nicolasfigb@gmail.com>
2023-01-07 13:03:24 -0500

    Base media queries

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     screens: {
       mobile: "375px",
       desktop: "1440px",
+      md: { max: "767px" },
     },
     extend: {
       fontFamily: {
@@ -19,6 +20,8 @@ module.exports = {
         "soft-red": "hsl(14, 88%, 65%)",
         "soft-violet": "hsl(273, 75%, 66%)",
         "soft-blue": "hsl(240, 73%, 65%)",
+        "very-dark-grayish-blue": "hsl(237, 12%, 33%)",
+        "dark-grayish-blue": "hsl(240, 6%, 50%)",
       },
     },
   },
