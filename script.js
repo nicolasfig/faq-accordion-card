8bbb82ce33ff9dab73c4dd68f9ef4712af100f24
Nicolas Figueroa <nicolasfigb@gmail.com>
2023-01-06 23:59:47 -0500

    Accordion toggle script

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 const questionBlocks = document.querySelectorAll(".question-answer");
+let lastSelected = null;
+
+const toggleAccordion = (answer) => {
+  answer.classList.toggle("answer-active");
+};
 
 for (i = 0; i < questionBlocks.length; i++) {
   const answer = questionBlocks[i].querySelector(".answer");
-
   questionBlocks[i].addEventListener("click", () => {
-    answer.classList.toggle("answer-active");
+    if (lastSelected) {
+      toggleAccordion(lastSelected);
+    }
+    toggleAccordion(answer);
+    lastSelected = answer;
   });
 }
